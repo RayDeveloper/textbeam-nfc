b7b94a29e939857be2a0927a8fd1ef012ada11f5
Raydon Davis <ray_5610@hotmail.com>
2023-06-10 23:52:03 -0400

    trimmed the lines and removed comments that were unnecessary.

--- app/src/main/java/com/example/textbeam_nfc/MainActivity.java
+++ app/src/main/java/com/example/textbeam_nfc/MainActivity.java
@@ -1,44 +1,26 @@
 package com.example.textbeam_nfc;
 
 
-import static android.nfc.NdefRecord.createMime;
-
-import static java.util.Date.parse;
-
 import android.annotation.SuppressLint;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.icu.text.SimpleDateFormat;
-import android.nfc.NfcManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcAdapter.CreateNdefMessageCallback;
 import android.nfc.NfcEvent;
-import android.os.Bundle;
 import android.os.Parcelable;
-import android.provider.Settings;
-import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-
-
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import java.nio.charset.Charset;
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity implements CreateNdefMessageCallback {
@@ -49,50 +31,30 @@ public class MainActivity extends AppCompatActivity implements CreateNdefMessage
     Date currentTime= new Date();
     Date currentTime_check= new Date();
     Date parsed_receivedDate= new Date();
-
-
     NfcAdapter nfcAdapter;
-    //TextView textView;
     EditText textBeam;
 
     @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
-
             nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-             if (nfcAdapter == null) {
+             if (nfcAdapter == null) {// Check for available NFC Adapter
                  Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
                 finish();
                 return;
             }//end if
-            if (!nfcAdapter.isEnabled()){
-                //NFC is not on.
+            if (!nfcAdapter.isEnabled()){//NFC is not on.
                     Toast.makeText(this, "NFC is disabled. Please turn on in settings.", Toast.LENGTH_LONG).show();
-
-            }else{
-            //NFC is  on.
+            }else{//NFC is  on.
             Toast.makeText(this, "NFC is enabled. You can begin transfer.", Toast.LENGTH_LONG).show();
-
-            }
-            // NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-
-            //  if (!nfcAdapter.isEnabled()) {
-            //    Intent intent = new Intent(Settings.ACTION_NFC_SETTINGS);
-            // NFC is available for device but not enabled
-            //}
-
+            }//end if
 
         textBeam = (EditText) findViewById(R.id.textBeam);// references the edittext for entering text
 
-            // Check for available NFC Adapter
-
-            // Register callback
-            nfcAdapter.setNdefPushMessageCallback(this, this);// if NFC is present then the call back will be used
+        nfcAdapter.setNdefPushMessageCallback(this, this);// if NFC is present then the call back will be used
             //Accepts a callback that contains a createNdefMessage() which is
             // called when a device is in range to beam data to. The callback lets you create the NDEF message only when necessary.
     }//end onCreate
@@ -104,6 +66,7 @@ public class MainActivity extends AppCompatActivity implements CreateNdefMessage
         currentTime = Calendar.getInstance().getTime();// uses the time from the device
         String UserString = textBeam.getText().toString()+" \nTimestamp:"+" " +sdf3.format(currentTime);// we get the text of the string that was entered in the edittext
         System.out.println(sdf3.format(currentTime));
+        System.out.println(UserString);//prints to log
         byte[] StringBytes = UserString.getBytes(); // the string has to be converted to bytes.
 
         NdefRecord ndefRecordOut = new NdefRecord( // the record has to be created first then the message
@@ -112,30 +75,9 @@ public class MainActivity extends AppCompatActivity implements CreateNdefMessage
                 new byte[] {}, //empty byte array
                 StringBytes);// text as bytes in the array
 
-        //NdefRecord outRecord = NdefRecord.createMime("com.example.textbeam_nfc", StringBytes);
-
         NdefMessage ndefMessageout = new NdefMessage(ndefRecordOut);
         return ndefMessageout;
     }
-
-   // @Override
-    //public void onResume() {
-    //    super.onResume();
-        // Check to see that the Activity started due to an Android Beam
-     //   if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-    //        processIntent(getIntent());
-     //   }
-
-    //    if (!nfcAdapter.isEnabled()){
-            //NFC is not on.
-    //        Toast.makeText(this, "NFC is disabled. Please turn on in settings.", Toast.LENGTH_LONG).show();
-
-     //   }else{
-            //NFC is  on.
-         //   Toast.makeText(this, "NFC is enabled. You can begin transfer.", Toast.LENGTH_LONG).show();
-
-       // }
-   // }
 
     @Override
     public void onResume() {
@@ -147,8 +89,7 @@ public class MainActivity extends AppCompatActivity implements CreateNdefMessage
         if (nfcAdapter == null)
             return;
         String[][] techList = new String[][] {};
-        nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
-
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null); //keeps the messaged received in the app
     }
 
     @Override
@@ -157,7 +98,7 @@ public class MainActivity extends AppCompatActivity implements CreateNdefMessage
         NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         if (nfcAdapter == null)
             return;
-        nfcAdapter.disableForegroundDispatch(this);
+        nfcAdapter.disableForegroundDispatch(this);//keeps the messaged received in the app
     }
 
     @SuppressLint("MissingSuperCall")
@@ -167,51 +108,34 @@ public class MainActivity extends AppCompatActivity implements CreateNdefMessage
         processIntent(intent);
     }
 
-
-
     @RequiresApi(api = Build.VERSION_CODES.N)
     void processIntent(Intent intent) {
-        //SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss", Locale.US);
         TextView textView = (TextView) findViewById(R.id.textView2);
 
-        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
-                NfcAdapter.EXTRA_NDEF_MESSAGES); //Extra containing an array of NdefMessage present on the discovered tag.
-        // only one message sent during the beam
-        NdefMessage msg = (NdefMessage) rawMsgs[0];
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES); //Extra containing an array of NdefMessage present on the discovered tag.
+        NdefMessage msg = (NdefMessage) rawMsgs[0];// only one message sent during the beam
         String msg1 = new String(msg.getRecords()[0].getPayload());
-        // record 0 contains the MIME type, record 1 is the AAR, if present
         textView.setText(new String(msg.getRecords()[0].getPayload())); // sets the textview to the message
-        // Declaring an empty string array
+        System.out.println(new String(msg.getRecords()[0].getPayload()));//prints to log
         String[] arr = null;
-        //int arr.size;
         arr = msg1.split(" ");
-        //int r = 0;
-       //int i= 0;
         try {
             System.out.println(arr[arr.length - 1]);
              String received_date = arr[arr.length - 1];
             parsed_receivedDate = sdf3.parse(received_date);
             System.out.println(parsed_receivedDate);
             currentTime_check = Calendar.getInstance().getTime();
-
         } catch (ParseException e) {
             e.printStackTrace();
         }
-
         String elapsed_Time = printDifference(parsed_receivedDate, currentTime_check);
-
-
-
-
-        Toast.makeText(this, elapsed_Time, Toast.LENGTH_LONG).show();
-
-
-    }
+        Toast.makeText(this, "Elapsed Time:"+ elapsed_Time, Toast.LENGTH_LONG).show();
+    }//end processIntent
 
 
     public String printDifference(Date startDate, Date endDate) { //method to calculate the difference between the sending time and received time.
-        //milliseconds
-        long different = endDate.getTime() - startDate.getTime();
+
+        long different = endDate.getTime() - startDate.getTime(); //milliseconds
 
         System.out.println("startDate : " + startDate);
         System.out.println("endDate : "+ endDate);
@@ -233,9 +157,8 @@ public class MainActivity extends AppCompatActivity implements CreateNdefMessage
 
         long elapsedSeconds = different / secondsInMilli;
 
-        return elapsedDays+"days,"+elapsedHours+"hours," +elapsedMinutes+"minutes,"+elapsedSeconds+"seconds" ;
-    }
+        Long elapsedMillies = different % secondsInMilli;
 
-
-
+        return elapsedDays+"days,"+elapsedHours+"hours," +elapsedMinutes+"minutes,"+elapsedSeconds+"seconds," ;
+    }//end printDifference
 }//end Main Activity
